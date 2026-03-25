c91570f82ac6864fd4ba2542c5b22d4ab397f090
LT <leo@leo.com>
2026-03-25 20:29:12 +0100

    fixed last errors of correction helpers

--- setup_python_env.sh
+++ setup_python_env.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 python3 -m venv .env
-source ~/.env/bin/activate
+source .env/bin/activate
 pip install -r requirements.txt
--- start_cmd.sh
+++ start_cmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-source ~/.env/bin/activate
+chmod 600 .vault_pass
+chmod 600 cloud-1_key
+chmod 600 cloud-1_key.pub
+source .env/bin/activate
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook  --private-key ./cloud-1_key -i inventory.yaml setup_cloud-1.yaml --vault-password-file .vault_pass
